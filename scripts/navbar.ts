24195885805aafb3c5b7297438c727c6a6b046d1
KittenSlayerExpress <hejsa2574@gmail.com>
2022-10-24 23:17:00 +0200

    Navbar kinda works, needs some work tho

---
+++ scripts/navbar.ts
@@ -0,0 +1,7 @@
+function toggleNavbar(){
+    let element = document.getElementById("navLinks");
+    if(element.style.display === "block"){
+        element.style.display = "none";}
+    else {
+        element.style.display = "block";}
+}
