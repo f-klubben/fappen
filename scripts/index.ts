fc45382b6b979333bb8002dfd4c6859f6a635ca4
nobogo <hejsa2574@gmail.com>
2022-10-26 14:41:20 +0200

    Added NavIndicator and fixed stuff

--- scripts/index.ts
+++ scripts/index.ts
@@ -1,6 +1,7 @@
 import {FaModule} from "./module";
 import config, {is_production} from "../config";
 import {init as init_stregsystem} from "./stregsystem";
+import {registerNavEvents} from "./navbar";
 
 (async () => {
     console.log(`Running in ${is_production ? "production" : "development"} mode.`);
@@ -9,6 +10,6 @@ import {init as init_stregsystem} from "./stregsystem";
     }
 
     await init_stregsystem();
-
+    await registerNavEvents();
     customElements.define("fa-module", FaModule);
 })()
