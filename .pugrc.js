6d4b502136ecca6d80e33866c6670807a96c0396
LowkeyCoding <lwalst20@student.aau.dk>
2022-10-28 16:01:07 +0200

    Added links page

--- .pugrc.js
+++ .pugrc.js
@@ -6,7 +6,17 @@ module.exports = {
         manifest: JSON.parse(readFileSync("./manifest.json", "utf-8")),
         navigation: {
             Frontpage: "./index.pug",
-            Stregsystem: "./stregsystem.pug"
+            Stregsystem: "./stregsystem.pug",
+            Links: "./links.pug"
+        },
+        links: {
+            Slack: "https://fklubben.slack.com",
+            Discord: "https://discord.gg/6DBvANjs3g",
+            Facebook: "https://www.facebook.com/fklub",
+            Github: "https://github.com/f-klubben",
+            Fiki: "https://fklub.dk",
+            Stregsystem: "https://stregsystem.fklub.dk"
+
         }
     }
 };
