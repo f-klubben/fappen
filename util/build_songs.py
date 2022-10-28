fc9a0a19613758fdfe89205dfa0544ead41bb28c
LowkeyCoding <lwalst20@student.aau.dk>
2022-10-28 13:35:23 +0200

    fix in case song dir is missing

--- util/build_songs.py
+++ util/build_songs.py
@@ -4,20 +4,22 @@ import json
 import shutil
 
 def main():
-   in_path = os.path.join(os.path.curdir, 'sangbog', 'sange')
-   out_path = os.path.join(os.path.curdir,  'pages', 'songbook', 'songs')
-   template_path = os.path.join(os.path.curdir, 'util', 'templates', 'tex_to.html')
-   pug_path = os.path.join(os.path.curdir, 'util', 'templates', 'songbook.pug')
-   black_list = ["enkortenlang.tex", "vendelboensfestsang.tex"]
-   files = os.listdir(in_path)
-   clean_folder(out_path)
-   pug_res = ""
-   with open(pug_path, encoding="utf-8") as f:
+    in_path = os.path.join(os.path.curdir, 'sangbog', 'sange')
+    out_path = os.path.join(os.path.curdir,  'pages', 'songbook', 'songs')
+    template_path = os.path.join(os.path.curdir, 'util', 'templates', 'tex_to.html')
+    pug_path = os.path.join(os.path.curdir, 'util', 'templates', 'songbook.pug')
+    black_list = ["enkortenlang.tex", "vendelboensfestsang.tex"]
+    if not os.path.exists(in_path):
+        os.mkdir(in_path)
+    files = os.listdir(in_path)
+    clean_folder(out_path)
+    pug_res = ""
+    with open(pug_path, encoding="utf-8") as f:
         pug_res = f.read()
-   pug_res += "\n"
-   if not os.path.exists(out_path):
-       os.mkdir(out_path)
-   for file in files:
+        pug_res += "\n"
+    if not os.path.exists(out_path):
+        os.mkdir(out_path)
+    for file in files:
         if file not in black_list:
             file_path = os.path.join(in_path, file)
             file_name = os.path.splitext(os.path.basename(file_path))[0]
@@ -29,9 +31,9 @@ def main():
             if song_name != None:
                 pug_res += f"        a(href='./songs/{file_name}.html') {song_name} #[br]\n"
 
-   with open( os.path.join(os.path.curdir,  'pages', 'songbook', 'index.pug'), encoding="utf-8", mode="w") as f:
-        f.write(pug_res)
-   remove_folder(os.path.join(os.path.curdir, 'sangbog'))
+    with open( os.path.join(os.path.curdir,  'pages', 'songbook', 'index.pug'), encoding="utf-8", mode="w") as f:
+            f.write(pug_res)
+    remove_folder(os.path.join(os.path.curdir, 'sangbog'))
 
 def clean_folder(path):
     files = os.listdir(path)
