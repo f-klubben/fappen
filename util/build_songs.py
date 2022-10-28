1741aae0ce5958c1ef89e26593eea6abea64e3a9
LowkeyCoding <lwalst20@student.aau.dk>
2022-10-28 13:31:58 +0200

    Test black list

--- util/build_songs.py
+++ util/build_songs.py
@@ -8,6 +8,7 @@ def main():
    out_path = os.path.join(os.path.curdir,  'pages', 'songbook', 'songs')
    template_path = os.path.join(os.path.curdir, 'util', 'templates', 'tex_to.html')
    pug_path = os.path.join(os.path.curdir, 'util', 'templates', 'songbook.pug')
+   black_list = ["enkortenlang.tex", "vendelboensfestsang.tex"]
    files = os.listdir(in_path)
    clean_folder(out_path)
    pug_res = ""
@@ -17,15 +18,16 @@ def main():
    if not os.path.exists(out_path):
        os.mkdir(out_path)
    for file in files:
-        file_path = os.path.join(in_path, file)
-        file_name = os.path.splitext(os.path.basename(file_path))[0]
-        out = os.path.join(out_path, file_name+".html")
+        if file not in black_list:
+            file_path = os.path.join(in_path, file)
+            file_name = os.path.splitext(os.path.basename(file_path))[0]
+            out = os.path.join(out_path, file_name+".html")
 
-        cmd = f"pandoc -s {file_path} -o {out} --template={template_path} --metadata title='{file_name}'"
-        os.system(cmd)
-        song_name = get_song_name(file_path)
-        if song_name != None:
-            pug_res += f"        a(href='./songs/{file_name}.html') {song_name} #[br]\n"
+            cmd = f"pandoc -s {file_path} -o {out} --template={template_path} --metadata title='{file_name}'"
+            os.system(cmd)
+            song_name = get_song_name(file_path)
+            if song_name != None:
+                pug_res += f"        a(href='./songs/{file_name}.html') {song_name} #[br]\n"
 
    with open( os.path.join(os.path.curdir,  'pages', 'songbook', 'index.pug'), encoding="utf-8", mode="w") as f:
         f.write(pug_res)
