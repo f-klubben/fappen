9eb02519e496dba5be8b0755fcd0929872c53c34
LowkeyCoding <loke0046@gmail.com>
2022-10-29 18:51:29 +0200

    Style changes + fixes
    Fixed paths in pug.
    Simplified generation of the songbook by generating a json file instead of the finished page.
    Changed style to make everything look like tables.
    Added the requirement that everything has to look like a table in style-guid.md.
    Ensured all styles sheets are in the scss format.
    Added a contributing guide (not stole from stregsystemet I swear).
    Added a toggle to enable and disable service-worker using the "disable_worker" environment variable.

--- .pugrc.js
+++ .pugrc.js
@@ -4,10 +4,12 @@ module.exports = {
     locals: {
         app_name: "F-Appen",
         manifest: JSON.parse(readFileSync("./manifest.json", "utf-8")),
+        songs: JSON.parse(readFileSync("./pages/songbook/songs.json", "utf-8")),
         navigation: {
-            Frontpage: "./index.pug",
-            Stregsystem: "./stregsystem.pug",
-            Links: "./links.pug"
+            Frontpage: "/pages/index.pug",
+            Stregsystem: "/pages/stregsystem.pug",
+            Songbook: "/pages/songbook/index.pug",
+            Links: "/pages/links.pug"
         },
         links: {
             Slack: "https://fklubben.slack.com",
@@ -17,6 +19,7 @@ module.exports = {
             Fiki: "https://fklub.dk",
             Stregsystem: "https://stregsystem.fklub.dk"
 
-        }
+        },
+        disable_worker: process.env.disable_worker === "true" || true,
     }
 };
---
+++ style-guid.md
@@ -0,0 +1,4 @@
+# Style guide
+Everything is styled like it's a table!  
+In case a table is used it should not look like a table!  
+The end!
