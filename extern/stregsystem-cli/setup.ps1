688656fbf39b29ef3a2c5162b8dd748715631fd5
Kresten Laust <krestenlaust@gmail.com>
2023-06-25 22:28:42 +0200

    Replaced project with submodule. Preserved commit history to the best of my ability.

--- extern/stregsystem-cli/setup.ps1
+++
@@ -1,9 +0,0 @@
-
-if (!(Test-Path -Path $PROFILE.CurrentUserAllHosts))
-{ New-Item -Type File -Path $PROFILE.CurrentUserAllHosts -Force }
-
-if (Test-Path -Path $PSScriptRoot\sts)
-{ $scriptitem = 'sts' }
-else 
-{ $scriptitem = 'main.py'} 
-"function sts {python $PSScriptRoot\$scriptitem `$args}" >> $PROFILE.CurrentUserAllHosts
